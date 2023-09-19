17e0ef06e42428397b6df2768181310de02d8a40
Dan Whitacre <whitacre.74@osu.edu>
2023-09-18 20:16:09 -0400

    move groupme client into plugin

--- src/plugins/groupme.ts
+++ src/plugins/groupme.ts
@@ -1,5 +1,53 @@
 import { Server } from '@hapi/hapi'
-import GroupMe from '../clients/groupme'
+import Wreck from '@hapi/wreck'
+
+export class GroupMe {
+  readonly server: Server
+  readonly baseUrl: string = process.env.GROUPME_BASEURL ?? 'https://api.groupme.com/v3/'
+  readonly client: typeof Wreck
+
+  constructor(server: Server) {
+    this.server = server
+
+    if (!process.env.GROUPME_TOKEN) throw new Error('missing GROUPME_TOKEN')
+
+    this.client = Wreck.defaults({
+      baseUrl: this.baseUrl,
+      headers: {
+        'X-Access-Token': process.env.GROUPME_TOKEN,
+      },
+    })
+  }
+
+  async call(method: string, url: string, options: { [_: string]: unknown }) {
+    try {
+      return this.client.request(method.toUpperCase(), url, options)
+    } catch (err) {
+      this.server.logger.error(err)
+      return false
+    }
+  }
+
+  async botPost(botId: string, message?: string, pictureurl?: string, options = {}) {
+    try {
+      const response = await this.call('post', 'bots/post', {
+        payload: {
+          bot_id: botId,
+          text: message,
+          picture_url: pictureurl,
+        },
+        ...options,
+      })
+      if (!response) throw new Error('groupme no response')
+
+      this.server.logger.debug({ status: response.statusCode, statusText: response.statusMessage }, 'groupme bots/post')
+      return { response }
+    } catch (err) {
+      this.server.logger.error(err, 'groupme failed to botPost')
+      return false
+    }
+  }
+}
 
 export default async function register(server: Server) {
   const groupme = new GroupMe(server)
--- src/types/hapi.d.ts
+++ src/types/hapi.d.ts
@@ -1,5 +1,5 @@
 import { Sheets } from '../plugins/sheets'
-import GroupMe from '../clients/groupme'
+import { GroupMe } from '../plugins/groupme'
 import Commands from '../clients/commands'
 
 declare module '@hapi/hapi' {
